4aad1c17149c27167f7a7ba9cf9a36020362bb9a
mshowkat <bangleblaze@gmail.com>
2019-07-18 15:05:54 +0600

    Updates

---
+++ precache-manifest.6bbca56b57ed8a5de9a15d0fae359a40.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49660383c7ec861d09c442e5d656f596",
+    "url": "/monster-inc/index.html"
+  },
+  {
+    "revision": "372226e96978a984216c",
+    "url": "/monster-inc/static/css/main.b4a49e8a.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/monster-inc/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "372226e96978a984216c",
+    "url": "/monster-inc/static/js/main.4de03bac.chunk.js"
+  },
+  {
+    "revision": "36453abbd7c426364191",
+    "url": "/monster-inc/static/js/runtime~main.2c49f10f.js"
+  }
+]);
